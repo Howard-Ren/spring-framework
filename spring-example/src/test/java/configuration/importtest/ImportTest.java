5d3e53ee3dc0d65bf3583bfe1e591d74b3c24c41
rhx <ren_hengxiao@126.com>
2022-08-16 22:34:13 +0800

    temp

--- spring-example/src/test/java/configuration/importtest/ImportTest.java
+++ spring-example/src/test/java/configuration/importtest/ImportTest.java
@@ -4,16 +4,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import java.util.Arrays;
+
 /**
  * @author rhx
  * @date 2022/8/15 11:51
  **/
 @Configuration
-@Import(ImportTest.class)
+//@Import(ImportTest.class)
 public class ImportTest {
 
 	public static void main(String[] args) {
 		final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ImportTest.class);
+		ImportTest bean = context.getBean(ImportTest.class);
+		System.out.println(Arrays.toString(bean.getClass().getInterfaces()));
 
 	}
 }
